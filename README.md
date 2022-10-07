dcc8a429c9e6d12cd4bbdc605ef8cfd3437c04bb
RPGlab <xingpeng.li.sg@gmail.com>
2022-10-07 12:55:26 -0500

    Initial Commitment

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# OHRES_sizing
- 
+## Optimal Sizing of Offshore Hybrid Renewable Energy Systems
+
+This program models the zeros emmision resilient offshore hybrid renewable energy system (OHRES). It is designed to optimize the size of the renewable units such that the relavent cost is minimized while meeting various constraints and requirements.
+
+
+### Input files:
+case16.dat
+* Case16 includes the load profile for 50MW offshore platform and the wind profile in a typical day at Gulf of Mexico. 
+
+
+### Codes: 
+1. SCUC.py  
+	* models the BESS, HESS and Wind turbine system.
+	* models the resilience duration time when there is no wind power.
+2. sensitivity.py   
+	* perform the sensitivity tests on the renewable units' prices.
+	* based on the SCUC.py
+
+
+### Environment Setting:
+To be able to run the code successfully, a python pyomo package and "Gurobi" solver are required.
+
+
+## Citation:
+If you use these codes for your work, please cite the following paper:
+
+Cunzhi Zhao and Xingpeng Li, “A 100% Renewable Energy System: Enabling Zero CO2 Emission Offshore Platforms”, *54th North American Power Symposium*, Salt Lake City, UT, USA, Oct. 2022.
+
+Paper website: <a class="off" href="/papers/Cunzhi-OffshoreMG-Sizing/"  target="_blank">https://rpglab.github.io/papers/Cunzhi-OffshoreMG-Sizing/</a>
+
+
+## Contributions:
+Cunzhi Zhao developed this program. Xingpeng Li supervised this work.
+
+
+## Contact:
+If you need any techinical support, please feel free to reach out to Cunzhi Zhao at czhao20@uh.edu.
+For collaboration, please contact Dr. Xingpeng Li at xli83@central.uh.edu.
+Website: https://rpglab.github.io/
+
+
+## License:
+This work is licensed under the terms of the <a class="off" href="https://creativecommons.org/licenses/by/4.0/"  target="_blank">Creative Commons Attribution 4.0 (CC BY 4.0) license.</a>
+
+
+## Disclaimer:
+The author doesn’t make any warranty for the accuracy, completeness, or usefulness of any information disclosed and the author assumes no liability or responsibility for any errors or omissions for the information (data/code/results etc) disclosed.
---
+++ sensitivity.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon June 20 15:42:20 2022
+
+@author: Cunzhi Zhao
+RPG Lab 
+University of Houston
+"""
+
+
+from pyomo.environ import *
+import pandas as pd
+import numpy as np
+#import gurobipy
+
+# instance the pyomo abstract model
+El = [500,600,700,800,900,1000,1100,1200]
+FC = [475, 550, 625, 700, 775, 850, 925, 1000]
+ResultData = np.zeros((8,8))
+for a in range(8):
+    for b in range(8):
+        ElC = El[a]
+        FCC = FC[b]
+        model = AbstractModel()
+        
+        ### set
+        model.PERIOD = Set()
+        
+        
+        ### Constant Parameters
+        model.WT_Cost = Param(default=20000000)
+        model.BESS_Cost = Param(default=350)
+        model.El_Cost = Param(default=ElC)
+        model.Comp_Cost = Param(default=40)
+        model.H2_kwh = Param(default=33)
+        model.FC_Cost = Param(default=FCC)
+        model.Cav = Param(default=35) # $/kg
+        model.WT_OM_Cost = Param(default=42000)
+        model.BESS_OM_Cost = Param(default=10)
+        model.El_OM_Cost = Param(default=0.02)
+        model.FC_OM_Cost = Param(default=0.013)
+        model.Cav_OM_Cost = Param(default = 44783)
+        model.BESSdegra =Param(default = 0.05)
+        model.El_effi =Param(default=0.7)
+        model.FC_effi =Param(default=0.6)
+        model.BESS_effi = Param(default=0.95)
+        model.lifetime = Param(default=20)
+        model.T_R = Param(default=6)
+        
+        
+        ### period parameters
+        model.Time_TotalPd = Param(model.PERIOD)
+        model.WT_Ava = Param(model.PERIOD)
+        model.BigM = Param(default = 1000)
+        
+        ### Variables
+        
+        model.USCHARGE = Var(model.PERIOD, within =Binary)
+        model.USDISCHARGE = Var(model.PERIOD, within =Binary)
+        model.PCHARGE = Var(model.PERIOD)
+        model.PDISCHARGE = Var(model.PERIOD)
+        model.WT = Var(within= NonNegativeIntegers)
+        model.BESS =  Var(within= PositiveReals)
+        model.El =  Var(within= PositiveReals)
+        model.FC =  Var(within= PositiveReals)
+        model.Cavern =  Var(within= PositiveReals)
+        model.P_FC = Var(model.PERIOD)
+        model.P_El = Var(model.PERIOD)
+        model.P_Curt = Var(model.PERIOD, within= NonNegativeReals)
+        model.ESSinitial = Var()
+        model.ESS = Var(model.PERIOD)
+        model.Cav_lvl = Var(model.PERIOD)
+        model.Cavinitial = Var()
+        model.Operation = Var()
+        
+        ###objective function
+        def objfunction(model):
+            totalcost = model.WT*(model.WT_Cost + model.WT_OM_Cost*model.lifetime) + model.BESS*(model.BESS_Cost(1+model.BESSdegra*model.lifetime)+ model.BESS_OM_Cost*model.lifetime) + \
+                model.El*(model.El_Cost*(1 + model.El_OM_Cost*model.lifetime) + model.Comp_Cost) + model.FC*model.FC_Cost*(1 + model.FC_OM_Cost*model.lifetime) + \
+                    model.Cavern*model.Cav + model.Cav_OM_Cost*model.lifetime
+            return totalcost
+        model.Cost = Objective(rule = objfunction, sense = minimize)
+        
+        ### Constrains
+        #power balance
+        
+        def powerbalance(model,j):
+            power = model.PDISCHARGE[j] + model.WT*model.WT_Ava[j] + model.P_FC[j]
+            power = power - (model.Time_TotalPd[j] + model.P_El[j] + model.PCHARGE[j] + model.P_Curt[j])
+            return  power == 0
+        model.Cons_powerblance = Constraint(model.PERIOD, rule = powerbalance)
+        
+        ### BESS 
+        def ESSbalance(model,j):
+            if j >=2:    
+                expr = model.ESS[j] -model.ESS[j-1] + model.PDISCHARGE[j]/model.BESS_effi - model.PCHARGE[j]*model.BESS_effi
+                return expr ==0
+            else:
+                return Constraint.Skip
+        model.Cons_ESSBalance = Constraint(model.PERIOD, rule = ESSbalance)
+        
+        def ESSinitial1(model, j):
+            expr = model.ESS[1] -model.ESSinitial + model.PDISCHARGE[1]/model.BESS_effi-model.PCHARGE[1]*model.BESS_effi
+            return expr == 0
+        model.Cons_ESSinitial1 = Constraint(model.PERIOD, rule = ESSinitial1)
+        
+        def ESSinitial2(model, j):
+            expr = 0.5*model.BESS - model.ESSinitial 
+            return expr == 0
+        model.Cons_ESSinitial2 = Constraint(model.PERIOD, rule = ESSinitial2)
+        
+        def ESSkeepon(model,j):
+            expr =  model.USCHARGE[j] + model.USDISCHARGE[j]
+            return expr <= 1
+        model.Cons_ESSKeepon = Constraint(model.PERIOD, rule = ESSkeepon)
+        
+        def Essend(model, j):
+            expr = model.ESS[24]
+            return expr == model.ESSinitial
+        model.Cons_Essend = Constraint(model.PERIOD, rule = Essend)
+        
+        def Esslimit1(model,j):
+            return  model.ESS[j] <= model.BESS
+        model.Cons_ESSlimit1 = Constraint(model.PERIOD,rule = Esslimit1)
+        
+        def Esslimit2(model,j):
+            return 0 <= model.ESS[j]
+        model.Cons_ESSlimit2 = Constraint(model.PERIOD,rule = Esslimit2)
+        
+        def PCHARGElimit1(model,j):
+            return model.USCHARGE[j]*0 <= model.PCHARGE[j] 
+        model.Cons_PCHARGElimit1 = Constraint(model.PERIOD,rule = PCHARGElimit1)
+        
+        def PCHARGElimit2(model,j):
+            return  model.PCHARGE[j] <= model.USCHARGE[j]*0.25*model.BESS
+        model.Cons_PCHARGElimit2 = Constraint(model.PERIOD,rule = PCHARGElimit2)
+        
+        def PDISCHARGElimit1(model,j):
+            return model.USDISCHARGE[j]*0 <= model.PDISCHARGE[j] 
+        model.Cons_PDISCHARGElimit1 = Constraint(model.PERIOD,rule = PDISCHARGElimit1)
+        
+        def PDISCHARGElimit2(model,j):
+            return model.PDISCHARGE[j] <= model.USDISCHARGE[j]*0.25*model.BESS
+        model.Cons_PDISCHARGElimit2 = Constraint(model.PERIOD,rule = PDISCHARGElimit2)
+        
+        #### Cavern
+        
+        def Cavbalance(model,j):
+            if j >=2:    
+                expr = model.Cav_lvl[j] -model.Cav_lvl[j-1] + model.P_FC[j]/model.FC_effi - model.P_El[j]*model.El_effi
+                return expr ==0
+            else:
+                return Constraint.Skip
+        model.Cons_CavBalance = Constraint(model.PERIOD, rule = Cavbalance)
+        
+        def Cavinitial1(model, j):
+            expr = model.Cav_lvl[1] -model.Cavinitial + model.P_FC[1]/model.FC_effi - model.P_El[1]*model.El_effi
+            return expr == 0
+        model.Cons_Cavinitial1 = Constraint(model.PERIOD, rule = Cavinitial1)
+        
+        def Cavinitial2(model, j):
+            expr = 0.5*model.Cavern - model.Cavinitial 
+            return expr == 0
+        model.Cons_Cavinitial2 = Constraint(model.PERIOD, rule = Cavinitial2)
+        
+        def CavernEnd(model, j):
+            expr = model.Cav_lvl[24]
+            return expr == model.Cavinitial
+        model.Cons_CavernEnd= Constraint(model.PERIOD, rule = CavernEnd)
+        
+        def Cavlimit1(model,j):
+            return  model.Cav_lvl[j] <= model.Cavern
+        model.Cons_Cavlimit1 = Constraint(model.PERIOD,rule = Cavlimit1)
+        
+        def Cavlimit2(model,j):
+            return 0 <= model.Cav_lvl[j]
+        model.Cons_Cavlimit2 = Constraint(model.PERIOD,rule = Cavlimit2)
+        
+        def PFC1(model,j):
+            return 0 <= model.P_FC[j]
+        model.Cons_PFC1 = Constraint(model.PERIOD,rule = PFC1)
+        
+        def PFC2(model,j):
+            return  model.P_FC[j] <= model.FC
+        model.Cons_PFC2 = Constraint(model.PERIOD,rule = PFC2)
+        
+        def PEl1(model,j):
+            return 0 <= model.P_El[j] 
+        model.Cons_PEl1 = Constraint(model.PERIOD,rule = PEl1)
+        
+        def PEl2(model,j):
+            return model.P_El[j] <= model.El
+        model.Cons_PEl2= Constraint(model.PERIOD,rule = PEl2)
+        
+        def PEl3(model,j):
+            return model.El*24*model.El_effi >= model.Cavern
+        model.Cons_PEl3= Constraint(model.PERIOD,rule = PEl3)
+        
+        
+        ## Resilience Models ### A
+        # def Resilience1(model):
+        #     return 0.25*model.BESS >= 60251
+        # model.Cons_Resilience1= Constraint(rule = Resilience1)
+        
+        # def Resilience2(model):
+        #     return model.BESS*model.BESS_effi >= 50000*model.T_R
+        # model.Cons_Resilience2= Constraint(rule = Resilience2)
+        
+        ### Resilience Models ### B
+        # def Resilience1(model):
+        #     return model.FC >= 60251
+        # model.Cons_Resilience1= Constraint(rule = Resilience1)
+        
+        # def Resilience2(model):
+        #     return model.Cavern*model.FC_effi >= 50000*model.T_R
+        # model.Cons_Resilience2= Constraint(rule = Resilience2)
+        
+        # Resilience Models ### C
+        def Resilience1(model):
+            return 0.25*model.BESS + model.FC >= 60251
+        model.Cons_Resilience1= Constraint(rule = Resilience1)
+        
+        def Resilience2(model):
+            return model.BESS*model.BESS_effi + model.Cavern*model.FC_effi >= 50000*model.T_R
+        model.Cons_Resilience2= Constraint(rule = Resilience2)
+        
+        
+        def OperationCost(model):
+            expr = model.WT*model.WT_OM_Cost*model.lifetime + model.BESS*model.BESS_OM_Cost*model.lifetime + \
+                model.El*model.El_Cost*( model.El_OM_Cost*model.lifetime) + model.FC*model.FC_Cost*(model.FC_OM_Cost*model.lifetime) + \
+                    + model.Cav_OM_Cost*model.lifetime
+            return expr == model.Operation
+        model.Constraint_OP_Cost = Constraint(rule = OperationCost)
+        
+        
+        # instance according on the dat file
+        SCUC_instance = model.create_instance('case16.dat')
+        
+        ### set the solver
+        SCUCsolver = SolverFactory('gurobi')
+        SCUCsolver.options.mipgap = 0.001
+        results = SCUCsolver.solve(SCUC_instance)
+        Data =[]
+        Result =[]
+        
+        print("\nresults.Solution.Status: " + str(results.Solution.Status))
+        print("\nresults.solver.status: " + str(results.solver.status))
+        print("\nresults.solver.termination_condition: " + str(results.solver.termination_condition))
+        print("\nresults.solver.termination_message: " + str(results.solver.termination_message))
+        print('\nminimize cost: ' + str(SCUC_instance.Cost()))
+        
+        for j in SCUC_instance.PERIOD:
+            X = [str(SCUC_instance.ESS[j]()/SCUC_instance.BESS()), str(SCUC_instance.PCHARGE[j]()),str(SCUC_instance.PDISCHARGE[j]()),str(SCUC_instance.Cav_lvl[j]()),str(SCUC_instance.P_FC[j]()),str(SCUC_instance.P_El[j]())]
+            Data.append(X)
+        
+        Data = pd.DataFrame(Data, columns=['ESS_SOC', 'CHARGE', 'DISCHARGE','Cavern_lvl','FC','El'])
+        print (Data)
+        
+        print('\Wind Turbin Amount: ' + str(SCUC_instance.WT()))
+        print('\BESS Capacity: ' + str(SCUC_instance.BESS()) + ' kWh')
+        print('\Eletrolyzer Power: ' + str(SCUC_instance.El()) + ' kW')
+        print('\Fuel Cell power: ' + str(SCUC_instance.FC()) + ' kW')
+        print('\Cavern Size: ' + str(SCUC_instance.Cavern()/40)+ ' kg')
+        
+        X = [SCUC_instance.WT(), SCUC_instance.BESS(),SCUC_instance.El(),SCUC_instance.FC(),SCUC_instance.Cavern()/SCUC_instance.H2_kwh(), \
+                  (SCUC_instance.Cost()-SCUC_instance.Operation())/1000000, SCUC_instance.Operation()/1000000,SCUC_instance.Cost()/1000000 ]
+        ResultData[a,b] = SCUC_instance.Cost()/1000000/8.76
+
+
+
